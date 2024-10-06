d51bb804bb61f954d585245f66c0451b9265f09b
Alex Aheyev <aheyevalex@gmail.com>
2024-10-06 23:35:34 +0000

    Tried Local and Persistent Flags, learned to invoke console messages on command

---
+++ todoapp/cmd/add.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		goggle, err := cmd.Flags().GetBool("goggle")
+		if err != nil {
+			fmt.Println("Error reading toggle flag:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("add called, Goggle is set to: %v\n", goggle)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- todoapp/cmd/root.go
+++ todoapp/cmd/root.go
@@ -1,30 +1,32 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Package cmd is the entry point for the application. It defines the root command and its subcommands.
 */
+
 package cmd
 
 import (
 	"os"
 
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cobra-cli",
+	Use:   "todoapp",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Long: `An application to manage your todo list.
+	Using command line interface, you can add, remove,
+	list and mark todo items as done.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		toggle, err := cmd.Flags().GetBool("toggle")
+		if err != nil {
+			fmt.Println("Error reading toggle flag:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Hello from rootCmd. Toggle is set to: %v\n", toggle)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -42,10 +44,8 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra-cli.yaml)")
-
+	rootCmd.PersistentFlags().BoolP("goggle", "g", false, "Help message for goggle")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
