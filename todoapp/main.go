d17aaf1bc805cfdd1264321c2068fe531946b4e8
Alex Aheyev <aheyevalex@gmail.com>
2024-10-24 18:50:21 +0000

    Added TODO for next update

--- todoapp/main.go
+++ todoapp/main.go
@@ -1,6 +1,8 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
+
+// TODO: Wrap the file reading and wrting logic into a separate package
 package main
 
 import "github.com/ArmoredStone/fuzzy-palm-tree/todoapp/cmd"
